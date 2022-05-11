333614ddd22013a9f8eb30032374b2875fb108b2
dafiazka <2031710110@student.polinema.ac.id>
2022-05-12 02:15:25 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/jobsheet8_/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.jobsheet8_
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+
+class MainActivity: FlutterActivity() {
+}
